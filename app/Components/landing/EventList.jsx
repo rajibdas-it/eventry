d00d2db0e4180c6cb93ea36e64db18ca07e81f35
rajib <rajibrad@gmail.com>
2024-11-06 11:43:26 +0600

    data set in event card

--- app/Components/landing/EventList.jsx
+++ app/Components/landing/EventList.jsx
@@ -3,11 +3,13 @@ import EventCard from "./EventCard";
 import { getAllEvents } from "@/db/quries";
 
 const EventList = async () => {
-  const items = await getAllEvents();
-  console.log(items);
+  const allEvents = await getAllEvents();
+  // console.log(allEvents);
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
-      <EventCard />
+      {allEvents.map((event) => (
+        <EventCard key={event?.id} event={event} />
+      ))}
     </div>
   );
 };
