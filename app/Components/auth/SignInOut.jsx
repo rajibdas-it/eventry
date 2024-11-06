5e372f18d71910fd08223b2ec8afcb1532e3bbf3
rajib <rajibrad@gmail.com>
2024-11-06 19:27:04 +0600

    logout function not done yet

--- app/Components/auth/SignInOut.jsx
+++ app/Components/auth/SignInOut.jsx
@@ -4,7 +4,24 @@ import React from "react";
 
 const SignInOut = () => {
   const { auth, setAuth } = useAuth();
-  return <div>{auth ? <h1>Rajib</h1> : <Link href="/login">Login</Link>}</div>;
+  const logout = () => {
+    setAuth(null);
+  };
+  return (
+    <div>
+      {auth ? (
+        <>
+          <span>Hello,{auth.name}</span>
+          <span className="mx-1">|</span>
+          <a className="cursor-pointer" onClick={logout}>
+            Logout
+          </a>
+        </>
+      ) : (
+        <Link href="/login">Login</Link>
+      )}
+    </div>
+  );
 };
 
 export default SignInOut;
