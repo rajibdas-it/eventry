7c43b07f57fa9e9c3a86422717bd3d36004f2155
rajib <rajibrad@gmail.com>
2024-11-07 12:58:56 +0600

    generate dynamic metadata

--- app/details/[id]/page.js
+++ app/details/[id]/page.js
@@ -4,6 +4,18 @@ import HeroSection from '@/app/Components/details/HeroSection';
 import { getEventDetails } from '@/db/quries';
 import React from 'react';
 
+export async function generateMetadata({ params: { id } }) {
+    const event = await getEventDetails(id)
+    return {
+        title: `Eventry - ${event?.name}`,
+        description: event?.details,
+        openGraph: {
+            images: [event?.imageUrl]
+        }
+    }
+
+}
+
 const EventDetailPage = async ({ params }) => {
     const { id } = await params
     const event = await getEventDetails(id)
